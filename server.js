2ec0b2e84a130c8be62dd3201c41f971061572af
Vrentadax <vrentadax@outlook.com>
2022-06-30 12:17:16 -0600

    deploying

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+/* This is the server.js file. It is setting up the server. */
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budgettracker";
 
 const app = express();
 
@@ -21,7 +22,6 @@ mongoose.connect(MONGODB_URI, {
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
